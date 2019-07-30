e492d8a2700d0720221f080d6699a509f358b589
aksh-t <tanno.ao@gmail.com>
2019-07-31 00:29:46 +0900

    add type guard

--- 4/src/1/3/test2.ts
+++ 4/src/1/3/test2.ts
@@ -1,3 +1,5 @@
 function getFormattedValue(value: number, unit: string | null = null){
+    const _value = value.toFixed(1)
+    if (unit === null) return `${_value}`
     return `${value.toFixed(1)} ${unit.toUpperCase()}`
 }
