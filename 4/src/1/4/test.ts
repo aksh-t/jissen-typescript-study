b554d60d6662cc205b8578a09ca19c75362d76d4
aksh-t <tanno.ao@gmail.com>
2019-07-31 00:35:18 +0900

    use weak type

--- 4/src/1/4/test.ts
+++ 4/src/1/4/test.ts
@@ -3,3 +3,18 @@ type User = {
     name?: string
 }
 function registerUser(user: User) { }
+
+const maybeUser = {
+    age: 26,
+    name: 'Taro',
+    gender: 'male'
+}
+const notUser = {
+    gender: 'male',
+    graduate: 'Tokyo'
+}
+registerUser(maybeUser)
+registerUser(notUser)
+
+registerUser({})
+registerUser()
