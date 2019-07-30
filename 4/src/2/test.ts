e69864f826c134f83596b39fb49612763ef9ea0a
aksh-t <tanno.ao@gmail.com>
2019-07-31 00:50:36 +0900

    Down Cast

--- 4/src/2/test.ts
+++ 4/src/2/test.ts
@@ -1,9 +1,11 @@
 const defaultTheme = {
-    backgroundColor: "orange",
-    borderColor: "red"
+    backgroundColor: "orange" as "orange",
+    borderColor: "red" as "red"
 }
 // 推論結果
 // const defaultTheme: {
-//     backgroundColor: string;
-//     borderColor: string;
+//     backgroundColor: "orange";
+//     borderColor: "red";
 // }
+
+defaultTheme.backgroundColor = "blue"
