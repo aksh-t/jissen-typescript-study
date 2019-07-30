32d6bf6236c44220517321e841441b2bba8b1bb0
aksh-t <tanno.ao@gmail.com>
2019-07-31 00:49:20 +0900

    Widening Literal Types

---
+++ 4/src/2/test.ts
@@ -0,0 +1,9 @@
+const defaultTheme = {
+    backgroundColor: "orange",
+    borderColor: "red"
+}
+// 推論結果
+// const defaultTheme: {
+//     backgroundColor: string;
+//     borderColor: string;
+// }
