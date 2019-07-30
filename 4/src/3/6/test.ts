9e578e0fc8b280b618e39c04e6c8eade68214eeb
aksh-t <tanno.ao@gmail.com>
2019-07-31 02:08:37 +0900

    filter type

--- 4/src/3/6/test.ts
+++ 4/src/3/6/test.ts
@@ -7,3 +7,12 @@ const users: (UserA436 | UserB436)[] = [
     { name: 'Hanako', graduate: 'Tokyo' }
 ]
 const filteredUsers = users.filter(user => 'graduate' in user)
+
+function filterUser(user: UserA436 | UserB436): user is UserB {
+    return 'graduate' in user
+}
+const filteredUsers2 = users.filter(filterUser)
+
+const filteredUsers3 = users.filter(
+    (user: UserA436 | UserB436): user is UserB436 => 'graduate' in user
+)
