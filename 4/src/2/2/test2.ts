93500e5b9b79d511050d61d9d0452a9c7f739c76
aksh-t <tanno.ao@gmail.com>
2019-07-31 01:07:31 +0900

    not limited property type

---
+++ 4/src/2/2/test2.ts
@@ -0,0 +1,14 @@
+type Answer = 'mighty' | 'lot' | 'few' | 'entirely'
+type User3 = {
+    name: string
+    enquete: { [k: string]: Answer}
+}
+const userA3: User3 = {
+    name: 'Taro',
+    enquete: {
+        exercise_habits: 'entirely',
+        time_of_sleeping: 'few'
+    }
+}
+const x3 = userA3.enquete['exercise_habits']
+const y3 = userA3.enquete['steps_per_day']
