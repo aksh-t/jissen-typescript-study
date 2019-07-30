53c449e7f02ed50dda061c2057d05e375a6b0f28
aksh-t <tanno.ao@gmail.com>
2019-07-31 00:25:11 +0900

    add comment of type

--- 4/src/1/3/test.ts
+++ 4/src/1/3/test.ts
@@ -1,6 +1,7 @@
 function getFormattedValue(value: number, unit = 'pt'){
     return `${value.toFixed(1)} ${unit.toUpperCase()}`
 }
+// function getFormattedValue(value: number | null): string (+1 overload)
 console.log(getFormattedValue(100))
 console.log(getFormattedValue(100, 'kg'))
 console.log(getFormattedValue(100, 0))
