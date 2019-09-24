46f9860374149351416b3707194f547567aaf7f3
aksh-t <tanno.ao@gmail.com>
2019-09-25 02:26:11 +0900

    9.1~2

---
+++ 9/2/src/server/routes.ts
@@ -0,0 +1,34 @@
+import Express from 'express'
+import createError from 'http-errors'
+import { Health } from '../types/api'
+
+export default (app: Express.Application) => {
+    app.use((req, res, next) => {
+        if (req.session !== undefined) {
+            if (req.session.count === undefined || req.session.count === null) {
+                req.session.count = 0
+            }
+        }
+    })
+
+    app.get('/', (req, res, next) => {
+        if (req.session) {
+            const data: { count: number} = { count: req.session.count }
+            res.render('index.ejs', data)
+            return
+        }
+        next(createError(404))
+    })
+
+    app.get('/ping', (req, res, next) => {
+        if (req.session) {
+            if (req.session.count !== undefined) {
+                req.session.count++
+                const data: Health = { message: 'pong', count: req.session.count }
+                res.send(data)
+                return
+            }
+        }
+        next(createError(401))
+    })
+}
